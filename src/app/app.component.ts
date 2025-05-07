485d6001beffe6acdaa9ff41097d5b53893b8f4a
Korawit Pimyotha <korawitpim123@gmail.com>
2025-05-07 12:29:21 +0700

    make second component V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { headerComponent } from "./header.component";
-
+import { headerComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [headerComponent],
+  imports: [headerComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  users = DUMMY_USERS
 }
