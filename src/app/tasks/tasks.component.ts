9c1be44924f4c88c221b00697171832d1b96c64a
Korawit Pimyotha <korawitpim123@gmail.com>
2025-05-10 14:28:17 +0700

    complete the new-task cpmponent

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -54,4 +55,16 @@ export class TasksComponent {
     this.isAddingTask = false;
   }
 
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.id,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+    this.isAddingTask = false;
+  }
+
 }
+
